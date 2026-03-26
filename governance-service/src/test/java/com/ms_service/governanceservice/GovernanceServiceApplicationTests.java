b9b76b7995d66ba933275c7d91f33f550791eac2
slimreaperII <blankoprog86@gmail.com>
2026-03-26 18:41:20 +0300

    initial project setup

---
+++ governance-service/src/test/java/com/ms_service/governanceservice/GovernanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_service.governanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

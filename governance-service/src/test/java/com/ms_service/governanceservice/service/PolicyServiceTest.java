330450e40b690d82ed452d74d5f13f03febee755
slimreaperII <blankoprog86@gmail.com>
2026-04-04 15:37:29 +0300

    implemented unit test for search policies endpoint

--- governance-service/src/test/java/com/ms_service/governanceservice/service/PolicyServiceTest.java
+++ governance-service/src/test/java/com/ms_service/governanceservice/service/PolicyServiceTest.java
@@ -7,6 +7,7 @@ import com.ms_service.governanceservice.kafka.KafkaProducer;
 import com.ms_service.governanceservice.policy.Policy;
 import com.ms_service.governanceservice.policy.Status;
 import com.ms_service.governanceservice.repository.PolicyRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -120,6 +121,130 @@ class PolicyServiceTest {
         verify(policyRepository, times(1)).findById(anyInt());
     }
 
+    @Nested
+    class searchPoliciesTest {
+        private Policy policy1;
+        private Policy policy2;
+        private Policy policy3;
+        private Policy policy4;
+
+        @BeforeEach
+        void setUp () {
+            policy1 = new Policy(1, "title 1", "desc 1", Status.DRAFT, "admin 1", LocalDateTime.now());
+            policy2 = new Policy(2, "title 2", "desc 2", Status.ACCEPTED, "admin 2", LocalDateTime.now());
+            policy3 = new Policy(3, "title 1", "desc 3", Status.PENDING_APPROVAL, "admin 1", LocalDateTime.now());
+            policy4 = new Policy(4, "title 4", "desc 4", Status.DRAFT, "admin 2", LocalDateTime.now());
+        }
+
+        @Test
+        void shouldReturnPoliciesWhenStatusNotProvided () {
+            //given
+            String title = "title 1";
+            String creator = "admin 1";
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title,creator)).thenReturn(List.of(policy1, policy3));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(title, creator, null);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy1.getPolicyId(), result.getFirst().getPolicyId());
+            assertEquals(policy3.getStatus(), result.get(1).getStatus());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title, creator);
+        }
+
+        @Test
+        void shouldReturnPoliciesWhenStatusIsProvided () {
+            //given
+            String title = "title 4";
+            String creator = "admin 2";
+            Status status = Status.DRAFT;
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCaseAndStatus(title, creator, status)).thenReturn(List.of(policy4));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(title, creator, status);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy4.getPolicyId(), result.getFirst().getPolicyId());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCaseAndStatus(title, creator, status);
+        }
+
+        @Test
+        void shouldReturnAllWhenNothingIsProvided () {
+            //given
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase("","")).thenReturn(List.of(policy1, policy2, policy3,policy4));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(null, null, null);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy1.getPolicyId(), result.getFirst().getPolicyId());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase("", "");
+        }
+
+        @Test
+        void shouldReturnPoliciesWhenOnlyTitleIsProvided () {
+            //given
+            String title = "title 1";
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title, "")).thenReturn(List.of(policy1, policy3));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(title, null, null);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy1.getTitle(), result.getFirst().getTitle());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title, "");
+        }
+
+        @Test
+        void shouldReturnPoliciesWhenOnlyCreatorIsProvided () {
+            //given
+            String creator = "admin 2";
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase("", creator)).thenReturn(List.of(policy2, policy4));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(null, creator, null);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy2.getCreatedBy(), result.getFirst().getCreatedBy());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase("", creator);
+        }
+
+        @Test
+        void shouldReturnPoliciesWhenOnlyStatusIsProvided (){
+            //given
+            Status status = Status.DRAFT;
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCaseAndStatus("", "", status)).thenReturn(List.of(policy1, policy4));
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(null, null, status);
+
+            //then
+            assertNotNull(result);
+            assertEquals(policy1.getPolicyId(), result.getFirst().getPolicyId());
+            assertEquals(policy1.getStatus(), result.getFirst().getStatus());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCaseAndStatus("", "", status);
+        }
+
+        @Test
+        void shouldReturnEmptyListWhenNoPoliciesFound() {
+            //given
+            String title = "title";
+            when(policyRepository.findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title, "")).thenReturn(List.of());
+
+            //when
+            List<PolicyResponse> result = policyService.searchPolicies(title, "", null);
+
+            //then
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+            verify(policyRepository, times(1)).findByTitleContainingIgnoreCaseAndCreatedByContainingIgnoreCase(title, "");
+        }
+    }
 
     @Nested
     class submitPolicyTest {
